e5ac15d286494c8fba1461f5c338a2b9c1a1267a
GA-Gamesdev <helpme704586@gmail.com>
2026-02-15 16:34:38 -0500

    Update achevescene.js

--- GAME/achevescene.js
+++ GAME/achevescene.js
@@ -4,48 +4,60 @@ scene("achevements",()=>{
 if(acheve.isUnlocked("f8an9dj")){
 add([
 rect(760,90,{radius:40}),
-
-pos(10,20),
+color(50, 168, 82),
+pos(10,60),
  
 ])
   add([
   sprite("acheve1"),
       scale(0.55),
           anchor("center"),
-      pos(84,65)
+      pos(84,110)
     
+  ])
+    add([
+     text("first world down!",{font:"happy"}),
+    scale(0.8),
+      anchor("center"),
+      pos(320,90)
+  ])
+
+    add([
+     text("beat world 1",{font:"happy"}),
+    scale(0.5),
+      anchor("center"),
+      pos(230,125)
   ])
 }else{
 add([
 rect(760,90,{radius:40}),
  color(99, 99, 99),
-pos(10,20)
+pos(10,60)
 ])
     add([
   sprite("lock"),
     scale(0.5),
       anchor("center"),
-      pos(84,65)
+      pos(60,105)
+  ])
+
+    add([
+     text("locked...",{font:"happy"}),
+    scale(0.8),
+      anchor("center"),
+      pos(210,90)
+  ])
+
+    add([
+     text("beat world 1",{font:"happy"}),
+    scale(0.5),
+      anchor("center"),
+      pos(230,125)
   ])
 }
+onKeyPress("escape",()=>{
+if(w1!="1"){
+localStorage.setItem("webformerlv", mapID);
+go("main menu")}
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
